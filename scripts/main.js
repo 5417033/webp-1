226205b210a2b3a435ea5ccb6449849582d0c1e5
5417033 <65747359+5417033@users.noreply.github.com>
2022-11-26 21:09:38 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 const myImage = document.querySelector('img');
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute('src');
-  if (mySrc === 'images/firefox-icon.png') {
+  if (mySrc === 'images/firefox.png') {
     myImage.setAttribute('src','images/firefox2.png');
   } else {
     myImage.setAttribute('src','images/firefox.png');

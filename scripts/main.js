1843b90fe0261707aab9bb15bfdd4b9c6580273d
5417033 <65747359+5417033@users.noreply.github.com>
2022-11-26 21:04:37 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,27 @@ myImage.onclick = () => {
     myImage.setAttribute('src','images/firefox-icon.png');
   }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  const myName = prompt('あなたの名前を入力してください。');
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Mozilla はかっこいいよ、${myName}`;
+  }
+}
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem('name');
+  myHeading.textContent = `Mozilla はかっこいいよ、${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+}
